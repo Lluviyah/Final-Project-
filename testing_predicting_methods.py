bc11d13e8adb3457a7b1d2cb1d530ce022de8d45
7jajohnson <jajohnson7@miners.utep.edu>
2023-12-07 10:21:09 -0700

    final_class_work

--- testing_predicting_methods.py
+++ testing_predicting_methods.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import statsmodels.api as sm
-from sklearn.metrics import accuracy_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, f1_score, classification_report, precision_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
@@ -13,10 +13,17 @@ import matplotlib.pyplot as plt
 import statsmodels.formula.api as sm_api
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
+from sklearn.preprocessing import StandardScaler
+
 df = pd.read_csv('hr_analytics.csv')
+df = df.drop_duplicates(keep='last')
+df = pd.get_dummies(df,dtype=int)
+object = StandardScaler()
+print(df[['average_montly_hours','time_spend_company','number_project']].describe())
+df[['average_montly_hours','time_spend_company','number_project']]= (df[['average_montly_hours','time_spend_company','number_project']]-df[['average_montly_hours','time_spend_company','number_project']].min())/(df[['average_montly_hours','time_spend_company','number_project']].max()-df[['average_montly_hours','time_spend_company','number_project']].min())
+print(df[['average_montly_hours','time_spend_company','number_project']].describe())
 Y = df['left']
 x_train, x_test, y_train, y_test = train_test_split(df.drop('left', axis=1), Y, test_size=0.3, random_state=1)
-df = pd.get_dummies(df,dtype=int)
 df.to_csv('dummy.csv')
 print(df)
 
@@ -28,11 +35,14 @@ models = {
     'Naive Bayes': GaussianNB(),
     'K-Nearest Neighbor': KNeighborsClassifier(n_neighbors=6)
 }
-
+model = sm.Logit(y_train, x_train)
+result = model.fit(method='newton')
+print(result)
 for name, model in models.items():
     print(f"{name}: ")
     model.fit(x_train, y_train)
     y_hat = model.predict(x_test)
     acc = accuracy_score(y_test, y_hat)
-    print(f"Accuracy: {acc}")
-    print(f"F1: {f1_score(y_test, y_hat)}")
+    # print(f"Accuracy: {acc}")
+    # print(f"F1: {f1_score(y_test, y_hat)}")
+    # print(f"Precision: {precision_score(y_test, y_hat)}")
